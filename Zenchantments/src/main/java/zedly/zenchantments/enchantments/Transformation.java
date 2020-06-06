babb42b9f569ebfb783e045f2e97068f514d8e15
Andres Puel <andreshpuel@gmail.com>
2020-06-05 18:01:49 -0700

    Stop blindly transforming, prevent undroppable equipment from dropping.

--- Zenchantments/src/main/java/zedly/zenchantments/enchantments/Transformation.java
+++ Zenchantments/src/main/java/zedly/zenchantments/enchantments/Transformation.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Tameable;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import zedly.zenchantments.CustomEnchantment;
 import zedly.zenchantments.Storage;
@@ -37,7 +38,11 @@ public class Transformation extends CustomEnchantment {
 
     @Override
     public boolean onEntityHit(EntityDamageByEntityEvent evt, int level, boolean usedHand) {
-        if (evt.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
+        if (evt.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK && evt.getCause() != EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK) {
+            return false;
+        }
+
+        if (!(evt.getEntity() instanceof LivingEntity)) {
             return false;
         }
         if (evt.getEntity() instanceof Tameable) {
@@ -45,8 +50,20 @@ public class Transformation extends CustomEnchantment {
                 return false;
             }
         }
-        if (evt.getEntity() instanceof LivingEntity
-                && ADAPTER.attackEntity((LivingEntity) evt.getEntity(), (Player) evt.getDamager(), 0)) {
+        if (evt.getEntity().getCustomName() != null) {
+            return false;
+        }
+        if (!evt.getEntity().isEmpty() || evt.getEntity().isInsideVehicle()) {
+            return false;
+        }
+        if (evt.getEntity().getScoreboardTags().size() == 0) {
+            return false;
+        }
+        if (!((LivingEntity) evt.getEntity()).hasAI()) {
+            return false;
+        }
+
+        if (ADAPTER.attackEntity((LivingEntity) evt.getEntity(), (Player) evt.getDamager(), 0)) {
             if (Storage.rnd.nextInt(100) > (100 - (level * power * 8))) {
                 LivingEntity newEnt = Storage.COMPATIBILITY_ADAPTER.TransformationCycle((LivingEntity) evt.getEntity(),
                         Storage.rnd);
@@ -58,25 +75,33 @@ public class Transformation extends CustomEnchantment {
                     Utilities.display(Utilities.getCenter(evt.getEntity().getLocation()), Particle.HEART, 70, .1f,
                             .5f, 2, .5f);
 
+                    double originalMaxHealth = ((LivingEntity) evt.getEntity()).getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
                     double originalHealth = ((LivingEntity) evt.getEntity()).getHealth();
-                    for (ItemStack stk : ((LivingEntity) evt.getEntity()).getEquipment().getArmorContents()) {
-                        if (stk.getType() != AIR) {
-                            newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), stk);
-                        }
+
+                    EntityEquipment equipment = ((LivingEntity) evt.getEntity()).getEquipment();
+                    if (equipment.getBootsDropChance() > 0.0f && equipment.getBoots().getType() != AIR) {
+                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), equipment.getBoots());
                     }
-                    if (((LivingEntity) evt.getEntity()).getEquipment().getItemInMainHand().getType() != AIR) {
-                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(),
-                                ((LivingEntity) evt.getEntity()).getEquipment().getItemInMainHand());
+                    if (equipment.getLeggingsDropChance() > 0.0f && equipment.getLeggings().getType() != AIR) {
+                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), equipment.getLeggings());
                     }
-                    if (((LivingEntity) evt.getEntity()).getEquipment().getItemInOffHand().getType() != AIR) {
-                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(),
-                                ((LivingEntity) evt.getEntity()).getEquipment().getItemInOffHand());
+                    if (equipment.getChestplateDropChance() > 0.0f && equipment.getChestplate().getType() != AIR) {
+                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), equipment.getChestplate());
+                    }
+                    if (equipment.getHelmetDropChance() > 0.0f && equipment.getHelmet().getType() != AIR) {
+                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), equipment.getHelmet());
+                    }
+                    if (equipment.getItemInMainHandDropChance() > 0.0f && equipment.getItemInMainHand().getType() != AIR) {
+                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), equipment.getItemInMainHand());
+                    }
+                    if (equipment.getItemInOffHandDropChance() > 0.0f && equipment.getItemInOffHand().getType() != AIR) {
+                        newEnt.getWorld().dropItemNaturally(newEnt.getLocation(), equipment.getItemInOffHand());
                     }
 
                     evt.getEntity().remove();
 
-                    newEnt.setHealth(Math.max(1,
-                            Math.min(originalHealth, newEnt.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue())));
+                    newEnt.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(originalMaxHealth);
+                    newEnt.setHealth(originalHealth);
 
                 }
             }
